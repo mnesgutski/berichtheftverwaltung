2896892c0c786c247ab47ff8f2133c4542693fcc
mnesgutski <maik.nesgutski@gmail.com>
2020-09-02 14:02:16 +0200

    eloquent relationships and check for reportbooks

---
+++ berichteverwaltung/resources/views/tests/reportBooks.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+</head>
+<body>
+	login war erfolgreich
+
+	@if(Auth::check())
+		hallo
+		{{auth()->user()->username}}
+		@if(null !== $reportBooks = auth()->user()->reportBooks())
+			@foreach($reportBooks as $reportBook)
+				<div class="col-3">heyo</div>
+			@endforeach
+		@endif
+
+		<div class="col-2">
+			<form action="" method="post">
+				<button type="submit">
+					create reportBook
+				</button>
+			</form>
+		</div>
+
+	@endif
+
+
+
+</body>
+</html>
