75e89eedc6444978bb378eb94071891991fa5269
mnesgutski <maik.nesgutski@gmail.com>
2020-09-02 12:46:17 +0200

    changed test folder structure

--- berichteverwaltung/app/Http/Controllers/TestController.php
+++ berichteverwaltung/app/Http/Controllers/TestController.php
@@ -12,7 +12,7 @@ class TestController extends Controller
 
     public function index(){
 
-    	return view('tests');
+    	return view('tests.login');
     }
 
     public function login(Request $request){
