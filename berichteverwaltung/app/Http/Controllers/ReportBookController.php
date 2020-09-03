2454a3caf8a560e6948b81fadf48a0466eec6ce6
mne <m.nesgutski@tk-world.de>
2020-09-03 22:13:35 +0200

    fix

--- berichteverwaltung/app/Http/Controllers/ReportBookController.php
+++ berichteverwaltung/app/Http/Controllers/ReportBookController.php
@@ -22,7 +22,7 @@ class ReportBookController extends Controller
         if (Auth::check()) {
             $reportBooks = auth()->user()->reportBooks;
             $data = [];
-            foreach($reportBooks as $reportBook) {
+            foreach ($reportBooks as $reportBook) {
                 $data[$reportBook->id] = [
                     'id' => $reportBook->id,
                     'owner' => $reportBook->owner,
@@ -41,9 +41,9 @@ class ReportBookController extends Controller
         return response()->json($response);
     }
 
-	/**
-	* creates reportbook from request data and returns in json format
-	**/
+    /**
+     * creates reportbook from request data and returns in json format
+     **/
     public function create(Request $request)
     {
         $response = [
@@ -53,33 +53,33 @@ class ReportBookController extends Controller
         ];
 
         /**$validator = Validator::make($request->all(), [
-            'apprenticeship_name' => 'required',
-            'begin_date' => 'required'
-        ]);
+         * 'apprenticeship_name' => 'required',
+         * 'begin_date' => 'required'
+         * ]);
+         *
+         * if($validator->fails()){
+         * $response['error_message'] = 'apprenticeship name and begin_date are required to work';
+         * return response()->json($response);
+         * }**/
 
-        if($validator->fails()){
-            $response['error_message'] = 'apprenticeship name and begin_date are required to work';
-            return response()->json($response);
-        }**/
+        if (Auth::check()) {
+            $reportBook = report_books::create([
+                'owner' => Auth::id(),
+                'apprenticeship_name' => ($request->apprenticeship_name !== null || $request->apprenticeship_name !== "") ? $request->apprenticeship_name : '',
+                'begin_date' => ($request->begin_date !== null || $request->begin_date !== "") ? $request->begin_date : '',
+                'end_date' => ($request->end_date !== null || $request->end_date !== "") ? $request->end_date : '',
+            ]);
 
-        if(Auth::check()){
-        	$reportBook = report_books::create([
-        		'owner' => Auth::id(),
-        		'apprenticeship_name' => ($request->apprenticeship_name !== null || $request->apprenticeship_name !== "") ? $request->apprenticeship_name :'',
-        		'begin_date' => ($request->begin_date !== null || $request->begin_date !== "") ? $request->begin_date :'',
-        		'end_date' => ($request->end_date !== null || $request->end_date !== "") ? $request->end_date :'',
-        	]);
+            $reportBook->save();
 
-        	$reportBook->save();
-
-        	$data[$reportBook->id] = [
-                    'owner' => $reportBook->owner,
-                    'name' => $reportBook->apprenticeship_name,
-                    'begin_date' => $reportBook->begin_date,
-                    'end_date' => $reportBook->end_date,
-                    'created_at' => $reportBook->created_at,
-                    'updated_at' => $reportBook->updated_at
-                ];
+            $data[$reportBook->id] = [
+                'owner' => $reportBook->owner,
+                'name' => $reportBook->apprenticeship_name,
+                'begin_date' => $reportBook->begin_date,
+                'end_date' => $reportBook->end_date,
+                'created_at' => $reportBook->created_at,
+                'updated_at' => $reportBook->updated_at
+            ];
             $response['error'] = false;
             $response['data'] = $data;
             return response()->json($response);
@@ -90,30 +90,31 @@ class ReportBookController extends Controller
         return response()->json($response);
     }
 
-    public function update(Request $request){
-    	$response = [
+    public function update(Request $request)
+    {
+        $response = [
             'error' => true,
             'error_message' => '',
             'data' => []
         ];
-        if(Auth::check()){
-        	$rBook = report_books::where('id', '=', $request->reportBookId)
-	        	->where('owner','=', Auth::id())
-	        	->first();
-	        $rbook->apprenticeship_name = ($request->apprenticeship_name !== null || $request->apprenticeship_name !== "") ? $request->apprenticeship_name : $rbook->apprenticeship_name;
-	        $rbook->begin_date = ($request->begin_date !== null || $request->begin_date !== "") ? $request->begin_date : $rbook->begin_date;
-	        $rbook->end_date = ($request->end_date !== null || $request->end_date !== "") ? $request->end_date : $rbook->end_date;
+        if (Auth::check()) {
+            $rBook = report_books::where('id', '=', $request->reportBookId)
+                ->where('owner', '=', Auth::id())
+                ->first();
+            $rbook->apprenticeship_name = ($request->apprenticeship_name !== null || $request->apprenticeship_name !== "") ? $request->apprenticeship_name : $rbook->apprenticeship_name;
+            $rbook->begin_date = ($request->begin_date !== null || $request->begin_date !== "") ? $request->begin_date : $rbook->begin_date;
+            $rbook->end_date = ($request->end_date !== null || $request->end_date !== "") ? $request->end_date : $rbook->end_date;
 
             $rBook->save();
 
-	        $data[$rBook->id] = [
-                    'owner' => $rBook->owner,
-                    'name' => $rBook->apprenticeship_name,
-                    'begin_date' => $rBook->begin_date,
-                    'end_date' => $rBook->end_date,
-                    'created_at' => $rBook->created_at,
-                    'updated_at' => $rBook->updated_at
-                ];
+            $data[$rBook->id] = [
+                'owner' => $rBook->owner,
+                'name' => $rBook->apprenticeship_name,
+                'begin_date' => $rBook->begin_date,
+                'end_date' => $rBook->end_date,
+                'created_at' => $rBook->created_at,
+                'updated_at' => $rBook->updated_at
+            ];
             $response['error'] = false;
             $response['data'] = $data;
             return response()->json($response);
@@ -123,20 +124,21 @@ class ReportBookController extends Controller
         return response()->json($response);
     }
 
-    public function delete(){
-    	$response = [
+    public function delete(Request $request)
+    {
+        $response = [
             'error' => true,
             'error_message' => '',
             'data' => []
         ];
-        if(Auth::check()){
-			$rBook = report_books::where('id', '=', $request->reportBookId)
-				->where('owner','=', Auth::id())
-				->first();
-			$rbook->delete();
-			$response['data']['delete'] = true;
-			$response['error'] = false;
-			return response()->json($response);
+        if (Auth::check()) {
+            $rBook = report_books::where('id', '=', $request->reportBookId)
+                ->where('owner', '=', Auth::id())
+                ->first();
+            $rBook->delete();
+            $response['data']['delete'] = true;
+            $response['error'] = false;
+            return response()->json($response);
         }
 
         $response['error_message'] = 'Something went wrong';
