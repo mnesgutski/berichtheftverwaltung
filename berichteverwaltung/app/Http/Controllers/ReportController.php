764d66d130eabaf8c591146f9b71bee849298f30
mnesgutski <maik.nesgutski@gmail.com>
2020-09-03 09:01:40 +0200

    get all reports of the specified reportbook

--- berichteverwaltung/app/Http/Controllers/ReportController.php
+++ berichteverwaltung/app/Http/Controllers/ReportController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\report_books;
+use App\reports;
 use Illuminate\Support\Facades\Auth;
 
 class ReportController extends Controller
 {
-    public function get()
+    public function get(Request $request)
     {
         $response = [
             'error' => true,
@@ -15,8 +17,29 @@ class ReportController extends Controller
             'data' => []
         ];
         if (Auth::check()) {
-            $reports = 
+            $rBook = report_books::where('id','=',$request->reportBookId)->first();
+            $reports = $rBook->reports;
+            $data = [];
+            foreach($reports as $rep) {
+                $data[$rep->id] = [
+                    'type' => $rep->type,
+                    'position' => $rep->postion,
+                    'begin_date' => $rep->begin_date,
+                    'end_date' => $rep->end_date,
+                    'department' => $rep->department,
+                    'company' => $rep->company,
+                    'hours_targeted' => $rep->hours_targeted,
+                    'created_at' => $rep->created_at,
+                    'updated_at' => $rep->updated_at,
+                ];
+            }
+            $response['error'] = false;
+            $response['data'] = $data;
+            return response()->json($response);
         }
+        
+        $response['error_message'] = 'Authentication needed';
+        return response()->json($response);
     }
 
     public function create(Request $request)
@@ -27,7 +50,7 @@ class ReportController extends Controller
             'data' => []
         ];
         if (Auth::check()) {
-
+            
         }
 
     }
