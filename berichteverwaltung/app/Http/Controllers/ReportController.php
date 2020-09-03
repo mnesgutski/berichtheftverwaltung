172fd6655788ab51896435b27a975db20fd72bbf
mne <m.nesgutski@tk-world.de>
2020-09-03 22:17:37 +0200

    check if not null

--- berichteverwaltung/app/Http/Controllers/ReportController.php
+++ berichteverwaltung/app/Http/Controllers/ReportController.php
@@ -18,23 +18,28 @@ class ReportController extends Controller
         ];
         if (Auth::check()) {
             $rBook = report_books::where('id', '=', $request->reportBookId)->first();
-            $reports = $rBook->reports;
-            $data = [];
-            foreach ($reports as $rep) {
-                $data[$rep->id] = [
-                    'type' => $rep->type,
-                    'position' => $rep->postion,
-                    'begin_date' => $rep->begin_date,
-                    'end_date' => $rep->end_date,
-                    'department' => $rep->department,
-                    'company' => $rep->company,
-                    'hours_targeted' => $rep->hours_targeted,
-                    'created_at' => $rep->created_at,
-                    'updated_at' => $rep->updated_at,
-                ];
+            if ($rBook !== null) {
+                $reports = $rBook->reports;
+                $data = [];
+                foreach ($reports as $rep) {
+                    $data[$rep->id] = [
+                        'type' => $rep->type,
+                        'position' => $rep->postion,
+                        'begin_date' => $rep->begin_date,
+                        'end_date' => $rep->end_date,
+                        'department' => $rep->department,
+                        'company' => $rep->company,
+                        'hours_targeted' => $rep->hours_targeted,
+                        'created_at' => $rep->created_at,
+                        'updated_at' => $rep->updated_at,
+                    ];
+                }
+
+                $response['error'] = false;
+                $response['data'] = $data;
+                return response()->json($response);
             }
-            $response['error'] = false;
-            $response['data'] = $data;
+            $response['error_message'] = 'No Reports available';
             return response()->json($response);
         }
 
