e038e39d23654e9910536fcd343733655c4c9688
mnesgutski <maik.nesgutski@gmail.com>
2020-09-04 21:59:16 +0200

    added some minor comments

--- berichteverwaltung/app/Http/Controllers/LoginController.php
+++ berichteverwaltung/app/Http/Controllers/LoginController.php
@@ -10,82 +10,82 @@ use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
-    //TODO: @MAIki https://laracasts.com/discuss/channels/laravel/how-to-give-user-login-option-in-laravel-socialite-azure
+  //TODO: @MAIki https://laracasts.com/discuss/channels/laravel/how-to-give-user-login-option-in-laravel-socialite-azure
 
-    /**
-     * Logs user in if User exists. Creates User if User doesnt exist
-     * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function login(Request $request)
-    {
-        /** $validator = Validator::make($request->all(), [
-            'username' => 'required',
-            'password' => 'required'
-        ]);**/
-        //@TODO:MAIK Fix Validation
+  /**
+  * Logs user in if User exists. Creates User if User doesnt exist
+  * @param Request $request
+  * @return \Illuminate\Http\JsonResponse
+  */
+  public function login(Request $request)
+  {
+    /** $validator = Validator::make($request->all(), [
+    *    'username' => 'required',
+    *    'password' => 'required'
+    *]);
+    **/
+    //@TODO:MAIK Fix Validation
 
-        $user = User::where('username', '=', $request->username)->first();
+    // if ($validator->fails()) {
+    //     $response['error_message'] = 'Username or Password incorrect. Password has to be at least 6 characters long.';
+    //     return response()->json($response);
+    // }
 
-        $credentials = ['username' => $request->username, 'password' => $request->password];
+    $user = User::where('username', '=', $request->username)->first();
+
+    $credentials = ['username' => $request->username, 'password' => $request->password];
+    $response = [
+      'error' => true,
+      'error_message' => null,
+      'data' => [
+        'login' => false,
+        'pw' => false,
+        'username' => '',
+        'registered' => false,
+      ]
+    ];
+
+    if ($user !== null) {
+      if (Auth::attempt($credentials)) {
         $response = [
-            'error' => true,
-            'error_message' => null,
-            'data' => [
-                'login' => false,
-                'pw' => false,
-                'username' => '',
-                'registered' => false,
-            ]
+          'error' => false,
+          'error_message' => null,
+          'data' => [
+            'login' => true,
+            'pw' => true,
+            'username' => $user->username,
+          ]
         ];
-
-        /**if ($validator->fails()) {
-            $response['error_message'] = 'Username or Password incorrect. Password has to be at least 6 characters long.';
-            return response()->json($response);
-        }**/
-
-        if ($user !== null) {
-            //if(Hash::check($request->password, $user->password))
-            if (Auth::attempt($credentials)) {
-                $response = [
-                    'error' => false,
-                    'error_message' => null,
-                    'data' => [
-                        'login' => true,
-                        'pw' => true,
-                        'username' => $user->username,
-                    ]
-                ];
-                return response()->json($response);
-            }
-            $response['error'] = false;
-            return response()->json($response);
-        }
-        return $this->createUser($request, $credentials);
-    }
-
-    /**
-     * creates User
-     * @param Request $request
-     * @param $credentials "Username and Password"
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function createUser(Request $request, $credentials)
-    {
-        $user = User::create([
-            'username' => $request->username,
-            'password' => Hash::make($request->password),
-        ]);
-
-        $user->save();
-        Auth::attempt($credentials);
-
-        $response['error'] = false;
-        $response['data']['login'] = true;
-        $response['data']['pw'] = true;
-        $response['data']['username'] = $user->username;
-        $response['data']['registered'] = true;
         return response()->json($response);
+      }
+      $response['error'] = false;
+      return response()->json($response);
     }
+    return $this->createUser($request, $credentials);
+  }
+
+  /**
+  * creates User
+  * @param Request $request
+  * @param $credentials "Username and Password"
+  * @return \Illuminate\Http\JsonResponse
+  */
+  public function createUser(Request $request, $credentials)
+  {
+    $user = User::create([
+      'username' => $request->username,
+      'password' => Hash::make($request->password),
+    ]);
+
+    $user->save();
+    Auth::attempt($credentials);
+
+    $response['error'] = false;
+    $response['data']['login'] = true;
+    $response['data']['pw'] = true;
+    $response['data']['username'] = $user->username;
+    $response['data']['registered'] = true;
+    return response()->json($response);
+  }
 
 }
