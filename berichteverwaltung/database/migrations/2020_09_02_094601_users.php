d28f593af45decf7e618597be0dffe3eefecebf7
mnesgutski <maik.nesgutski@gmail.com>
2020-09-05 13:56:38 +0200

    default value for azure_id

--- berichteverwaltung/database/migrations/2020_09_02_094601_users.php
+++ berichteverwaltung/database/migrations/2020_09_02_094601_users.php
@@ -16,7 +16,7 @@ class Users extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email')->nullable();
-            $table->string('azure_id')->nullable();
+            $table->string('azure_id')->nullable()->default(null);
             $table->string('username')->default(null);
             $table->string('password')->default(null)->nullable();
             $table->timestamps();
